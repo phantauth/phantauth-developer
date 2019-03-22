3f92f0ada537206c0ad0fa66d187c172a77a04e8
Ivan SZKIBA <szkiba@users.noreply.github.com>
2019-03-22 22:27:01 +0100

    Use environment variables in gatsby build

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,10 @@ module.exports = {
     title: `PhantAuth Developer`,
     description: `Random User Generator + OpenID Connect Provider. Like Lorem Ipsum, but for user accounts and authentication.`,
     author: `Iván Szkiba`,
+    domain: process.env.PHANTAUTH_DOMAIN,
+    serviceUri: process.env.PHANTAUTH_SERVICE_URI,
+    developerPortalUri: process.env.PHANTAUTH_DEVELOPER_PORTAL_URI,
+    defaultTenantUri: process.env.PHANTAUTH_DEFAULT_TENANT_URI
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
